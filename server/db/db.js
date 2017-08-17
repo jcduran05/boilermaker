05d4edc87089294bf8b85c3f4b2b5ed067a24cdd
jcduran05 <jcduran05@gmail.com>
2017-08-16 21:48:36 -0700

    Added support for db name/user/password env variables. Windows support through cross-env.

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,11 @@
 const Sequelize = require('sequelize')
-const db = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://localhost:5432/boilermaker', {
-    logging: false
-  }
-)
+if (process.env.NODE_ENV !== 'production') require('../../secrets')
+
+const name = process.env.DATABASE_NAME
+const username = (process.env.DATABASE_USERNAME ? process.env.DATABASE_USERNAME : '')
+const password = (process.env.DATABASE_PASSWORD ? process.env.DATABASE_PASSWORD : '')
+const url = process.env.DATABASE_URL || (name && username && password ?
+    `postgres://${username}:${password}@localhost:5432/${name}` : `postgres://localhost:5432/boilermaker`)
+
+const db = new Sequelize(url, { logging: false })
 module.exports = db
